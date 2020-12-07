c8a71f39d6f0c343efd6da5e79d5ada2f00ebd26
moroojF <morooj.f2020@gmail.com>
2020-12-07 17:34:48 +0300

    part One with preparations of the next parts

---
+++ src/components/AddForm.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const AddForm = () => {
+    const [title, setMyTitle] = useState("");
+    const [price, setMyPrice] = useState(1);
+    const [desc, setMyDesc] = useState("");
+
+    const myCreate = e => {
+        e.preventDefault();
+        axios.post("http://localhost:8000/api/product/new", {
+            title,
+            price,
+            desc
+        }).then(res => console.log(res))
+            .catch(err => console.log(err))
+    }
+    return (
+        <div className="row d-flex justify-content-center my-4">
+            <div className="card text-left">
+                <div className="card-body">
+                    <h4 className="card-title">New Product</h4>
+                    <div className="card-text">
+                        <form onSubmit={myCreate}>
+                            <div className="form-grpup">
+                                <label>Title:</label>
+                                <input type="text" className="form-control" onChange={e => setMyTitle(e.target.value)} value={title} />
+                            </div>
+                            <div className="form-grpup">
+                                <label>Price:</label>
+                                <input type="number" className="form-control" onChange={e => setMyPrice(e.target.value)} value={price} />
+                            </div>
+                            <div className="form-grpup">
+                                <label>Description:</label>
+                                <input type="text" className="form-control" onChange={e => setMyDesc(e.target.value)} value={desc} />
+                            </div>
+                            <div className="form-grpup">
+                                <input type="submit" className="btn btn-outline-secondary my-4" value="Create" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default AddForm;
---
+++ src/views/Main.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AddForm from '../components/AddForm';
+export default () => {
+    return (
+        <div className="container">
+            <AddForm />
+        </div>
+    )
+}
